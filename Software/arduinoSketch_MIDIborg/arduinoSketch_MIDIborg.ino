95a54ae054d253aeddf27f73ebf958e81dec4cb1
gis667en11 <gis667en11@gmail.com>
2022-01-15 13:20:39 -0500

    Structuring program

--- Software/arduinoSketch_MIDIborg/arduinoSketch_MIDIborg.ino
+++ Software/arduinoSketch_MIDIborg/arduinoSketch_MIDIborg.ino
@@ -1,10 +1,21 @@
-#include <SPI.h>
+// #################### Libraries ####################
+
 #include <MCP23S17.h>
 
+// #################### Constants ######################
+
+// ##################### STRUCT ########################
+
+
+
+// ############### Variable Definition #################
+
+MCP23S17 Bank1(&SPI, chipSelect, 0);
+MCP23S17 Bank2(&SPI, chipSelect, 1);
 
 void setup() {
   // put your setup code here, to run once:
-
+  
 }
 
 void loop() {
